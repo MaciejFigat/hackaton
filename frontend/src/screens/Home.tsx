b1eeea350c14d6b9556200c27c04b3f6d6e7311e
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-10-23 11:39:05 +0200

    basic intro v0.1

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { LinkContainer } from 'react-router-bootstrap'
-import CardComponent from '../components/CardComponent'
+// import { LinkContainer } from 'react-router-bootstrap'
+// import CardComponent from '../components/CardComponent'
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
