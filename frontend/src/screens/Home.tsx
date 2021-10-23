cb507fcb9a8074b42c865c95d4e83937b2ab705c
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-10-23 15:58:25 +0200

    changed about content v.03

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -89,25 +89,3 @@ const Home: React.FC<HomeProps> = () => {
   )
 }
 export default Home
-
-{
-  /* <CardComponent
-variantCard='success'
-title='Budowanie zaufania'
-description='Korzystne będzie budowanie wzajemnego zaufania.'
-link=''
-linkDescription='Więcej info'
-variantButton='danger'
-/> */
-}
-
-{
-  /* <CardComponent
-variantCard='dark'
-title='Przeciwdziałanie obojętności'
-description='Strach przed oszustwem, błędne praktyki i zaniedbania powodują wzrastającą obojętność klienta.'
-link=''
-linkDescription='Więcej info'
-variantButton='warning'
-/> */
-}
