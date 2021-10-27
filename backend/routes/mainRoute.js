23da646c219172effd83f6f9eba0152a992ef4ea
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-10-27 22:26:04 +0200

    basic express server setup - routes v0.2

--- backend/routes/mainRoute.js
+++ backend/routes/mainRoute.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const needle = require('needle')
 
-router.get('/', (req, res) => {
-  res.json({ success: true })
+const API_KEY_VALUE = process.env.API_KEY_VALUE
+const API_URL = process.env.API_URL
+
+// needle returns a promis, hence it should be an async function
+router.get('/', async (req, res) => {
+  try {
+    const apiResponse = await needle(
+      'get',
+      `${API_URL}current?access_key=${API_KEY_VALUE}&query=Warsaw`
+    )
+    const data = apiResponse.body
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
 })
+
+// http://api.weatherstack.com/current
+//     ? access_key = API_KEY_VALUE
+//     & query = New York
 
 module.exports = router
