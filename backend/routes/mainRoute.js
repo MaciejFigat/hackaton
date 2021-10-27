166018c57306ab3baad562127bb3c7869a377f00
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-10-27 23:28:51 +0200

    basic express server setup - routes v0.3

--- backend/routes/mainRoute.js
+++ backend/routes/mainRoute.js
@@ -3,14 +3,22 @@ const router = express.Router()
 const needle = require('needle')
 
 const API_KEY_VALUE = process.env.API_KEY_VALUE
+const API_KEY_NAME = process.env.API_KEY_NAME
 const API_URL = process.env.API_URL
+const API_QUERY = process.env.API_QUERY
 
 // needle returns a promis, hence it should be an async function
 router.get('/', async (req, res) => {
   try {
+    // console.log(url.parse(req.url, true).query)
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+    })
+    // so I can use ${params} instead of ${API_KEY_NAME}=${API_KEY_VALUE}
+
     const apiResponse = await needle(
       'get',
-      `${API_URL}current?access_key=${API_KEY_VALUE}&query=Warsaw`
+      `${API_URL}current?${params}&query=Warsaw`
     )
     const data = apiResponse.body
     res.status(200).json(data)
@@ -19,6 +27,8 @@ router.get('/', async (req, res) => {
   }
 })
 
+// `${API_URL}current?${params}&query=Warsaw`
+// `${API_URL}current?access_key=${API_KEY_VALUE}${params}`
 // http://api.weatherstack.com/current
 //     ? access_key = API_KEY_VALUE
 //     & query = New York
